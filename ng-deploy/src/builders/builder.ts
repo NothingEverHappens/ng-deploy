e2ef0218e77d31272747dc11cd93a4189b5f1fa6
Minko Gechev <mgechev@google.com>
2019-03-05 15:17:15 -0800

    Run the builder with Architect CLI

--- ng-deploy/src/builders/builder.ts
+++ ng-deploy/src/builders/builder.ts
@@ -1,11 +1,11 @@
 import { index2 as architect } from '@angular-devkit/architect';
-import { Schema as DeploySchema } from './schema';
+// import { Schema as DeploySchema } from './schema';
 import deploy from './actions/deploy';
 
 
 // Call the createBuilder() function to create a builder. This mirrors
 // createJobHandler() but add typings specific to Architect Builders.
-export default architect.createBuilder<DeploySchema>((options, context) => {
+export default architect.createBuilder<any>((_, __) => {
     // The project root is added to a BuilderContext.
     deploy();
     return {success: true};
