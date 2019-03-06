fd79e8cd43a9691fef632ce58f5167f88874a190
Minko Gechev <mgechev@google.com>
2019-03-05 17:30:21 -0800

    Use newest devkit & improve ng add

--- ng-deploy/src/builders/actions/deploy.ts
+++ ng-deploy/src/builders/actions/deploy.ts
@@ -1,4 +1,5 @@
 import { BuilderContext } from '@angular-devkit/architect/src/index2';
+import { join } from 'path';
 
 const tools = require('firebase-tools');
 
@@ -10,10 +11,27 @@ export default function deploy(context: BuilderContext) {
         return tools.login();
     })
     .then(() => {
-        context.logger.info('Building your application' + context.workspaceRoot);
+        context.logger.info('Building your application');
         if (!context.target) {
             throw new Error('Cannot execute the build target')
         }
-        return context.scheduleTarget({ target: 'build', project: context.target.project });
+        return Promise.resolve({ result: Promise.resolve(42)})//context.scheduleTarget({ target: 'build', project: context.target.project, configuration: 'production' })
+            .then(res => res.result)
+            .then(() => {
+                if (!context.target) {
+                    throw new Error('Cannot execute the build target')
+                }
+                context.logger.info(join(context.workspaceRoot, 'dist', context.target!.project));
+                return tools.deploy({
+                    project: 'foo',
+                    token: process.env.FIREBASE_TOKEN,
+                    cwd: join(context.workspaceRoot, 'dist', context.target.project)
+                });
+            })
+            .then(() => {
+                context.logger.info('Successful deployment');
+            }).catch(e => {
+                console.log(e);
+            })
     })
 }
--- ng-deploy/src/ng-deploy/index.ts
+++ ng-deploy/src/ng-deploy/index.ts
@@ -2,6 +2,29 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // import {addPackageJsonDependency,  NodeDependencyType} from 'schematics-utilities';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
+const firebaseJson = {
+  hosting: {
+    public: 'dist/',
+    ignore: [
+      'firebase.json',
+      '**/.*',
+      '**/node_modules/**'
+    ],
+    rewrites: [
+      {
+        source: '**',
+        destination: '/index.html'
+      }
+    ]
+  }
+};
+
+const overwriteIfExists = (tree: Tree, path: string, content: string) => {
+  if (tree.exists(path)) tree.overwrite(path, content);
+  else tree.create(path, content);
+};
+
+
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function ngDeploy(_options: any): Rule {
@@ -17,6 +40,8 @@ export function ngDeploy(_options: any): Rule {
     // addPackageJsonDependency(tree, ngDeploy);
 
     _context.addTask(new NodePackageInstallTask());
+    overwriteIfExists(tree, 'firebase.json', JSON.stringify(firebaseJson));
+    overwriteIfExists(tree, '.firebaserc', '{}')
 
     return tree;
   };
