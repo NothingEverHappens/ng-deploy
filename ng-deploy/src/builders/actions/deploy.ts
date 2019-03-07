1d1eba6b29571169dc4b586d3b95f0e9eecb20b6
Minko Gechev <mgechev@google.com>
2019-03-06 17:11:17 -0800

    Use async/await

--- ng-deploy/src/builders/actions/deploy.ts
+++ ng-deploy/src/builders/actions/deploy.ts
@@ -2,34 +2,28 @@ import { BuilderContext } from '@angular-devkit/architect/src/index2';
 
 const tools = require('firebase-tools');
 
-export default function deploy(context: BuilderContext) {
-  return tools
-    .list()
-    .catch(() => {
-      context.logger.warn("🚨 You're not logged into Firebase. Logging you in...");
-      return tools.login();
-    })
-    .then(() => {
-      context.logger.info('📦 Building your application...');
-      if (!context.target) {
-        throw new Error('Cannot execute the build target');
-      }
-      return context
-        .scheduleTarget({ target: 'build', project: context.target.project, configuration: 'production' })
-        .then(res => res.result)
-        .then(() => {
-          if (!context.target) {
-            throw new Error('Cannot execute the build target');
-          }
-          return tools.deploy({
-            cwd: context.workspaceRoot
-          });
-        })
-        .then((success: { hosting: string }) => {
-          context.logger.info(`🚀 Your application is now available at https://${success.hosting.split('/')[1]}.firebaseapp.com/`);
-        })
-        .catch(e => {
-          context.logger.error(e);
-        });
+export default async function deploy(context: BuilderContext) {
+  try {
+    await tools.list();
+  } catch (e) {
+    context.logger.warn("🚨 You're not logged into Firebase. Logging you in...");
+    await tools.login();
+  }
+  context.logger.info('📦 Building your application...');
+  if (!context.target) {
+    throw new Error('Cannot execute the build target');
+  }
+  const run = await context.scheduleTarget({ target: 'build', project: context.target.project, configuration: 'production' });
+  await run.result;
+  if (!context.target) {
+    throw new Error('Cannot execute the build target');
+  }
+  try {
+    const success = await tools.deploy({
+      cwd: context.workspaceRoot
     });
+    context.logger.info(`🚀 Your application is now available at https://${success.hosting.split('/')[1]}.firebaseapp.com/`);
+  } catch (e) {
+    context.logger.error(e);
+  }
 }
--- ng-deploy/src/builders/actions/init.ts
+++ ng-deploy/src/builders/actions/init.ts
@@ -1,15 +1,3 @@
-import { execSync, spawnSync } from 'child_process';
-
-export default function init() {
-  // TODO(kirjs): Is there a  better way?
-  const bin = execSync('(cd ' + __dirname + ' && npm bin)')
-    .toString()
-    .trim();
-  const firebaseBin = bin + '/firebase';
-  spawnSync(firebaseBin, ['login'], { stdio: 'inherit' });
-  spawnSync(firebaseBin, ['init', 'hosting'], { stdio: 'inherit' });
-}
-
 const firebase = require('firebase-tools');
 
 export const listProjects = () => {
