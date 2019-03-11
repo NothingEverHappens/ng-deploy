8f6b725470c7e1e5d41983d7d9871ea5e98e2c9a
kirjs <kirill@kcherkashin.org>
2019-03-11 12:23:42 -0400

    Add multiapp support

--- ng-deploy/src/builders/actions/deploy.ts
+++ ng-deploy/src/builders/actions/deploy.ts
@@ -1,29 +1,41 @@
 import { BuilderContext } from '@angular-devkit/architect/src/index2';
-import { FirebaseTools } from '../../ng-deploy/types';
+import { FirebaseTools } from '../../shared/types';
 
-export default async function deploy(firebaseTools: FirebaseTools, context: BuilderContext, projectRoot: string) {
-  try {
-    await firebaseTools.list();
-  } catch (e) {
-    context.logger.warn("🚨 You're not logged into Firebase. Logging you in...");
-    await firebaseTools.login();
-  }
-  if (!context.target) {
-    throw new Error('Cannot execute the build target');
-  }
+export default async function deploy(firebaseTools: FirebaseTools, context: BuilderContext, projectRoot: string, firebaseProject?: string) {
+    if (!firebaseProject) {
+        throw new Error('Cannot find firebase project for your app in .firebaserc');
+    }
 
-  context.logger.info(`📦 Building "${context.target.project}"`);
+    try {
+        await firebaseTools.list();
+    } catch (e) {
+        context.logger.warn("🚨 You're not logged into Firebase. Logging you in...");
+        await firebaseTools.login();
+    }
+    if (!context.target) {
+        throw new Error('Cannot execute the build target');
+    }
 
-  const run = await context.scheduleTarget({ target: 'build', project: context.target.project, configuration: 'production' });
-  await run.result;
-  if (!context.target) {
-    throw new Error('Cannot execute the build target');
-  }
+    context.logger.info(`📦 Building "${context.target.project}"`);
 
-  try {
-    const success = await firebaseTools.deploy({ cwd: projectRoot });
-    context.logger.info(`🚀 Your application is now available at https://${success.hosting.split('/')[1]}.firebaseapp.com/`);
-  } catch (e) {
-    context.logger.error(e);
-  }
+    const run = await context.scheduleTarget({
+        target: 'build',
+        project: context.target.project,
+        configuration: 'production'
+    });
+    await run.result;
+
+    try {
+        await firebaseTools.use(firebaseProject, {project: firebaseProject});
+    } catch (e) {
+        throw new Error(`Cannot select firebase project '${firebaseProject}'`);
+    }
+
+
+    try {
+        const success = await firebaseTools.deploy({only: 'hosting:' + context.target.project, cwd: projectRoot});
+        context.logger.info(`🚀 Your application is now available at https://${success.hosting.split('/')[1]}.firebaseapp.com/`);
+    } catch (e) {
+        context.logger.error(e);
+    }
 }
--- ng-deploy/src/builders/builder.ts
+++ ng-deploy/src/builders/builder.ts
@@ -2,21 +2,25 @@ import { BuilderContext, BuilderOutput, createBuilder } from '@angular-devkit/ar
 import { NodeJsSyncHost } from '@angular-devkit/core/node';
 // import { Schema as DeploySchema } from './schema';
 import deploy from './actions/deploy';
-import { normalize, experimental, join } from '@angular-devkit/core';
+import { experimental, join, normalize } from '@angular-devkit/core';
+import { getFirebaseProjectName } from '../shared/utils';
 
 // Call the createBuilder() function to create a builder. This mirrors
 // createJobHandler() but add typings specific to Architect Builders.
 export default createBuilder<any>(
-  async (_: any, context: BuilderContext): Promise<BuilderOutput> => {
-    // TODO: handle failure
-    // The project root is added to a BuilderContext.
-    const root = normalize(context.workspaceRoot);
-    const workspace = new experimental.workspace.Workspace(root, new NodeJsSyncHost())
-    await workspace.loadWorkspaceFromHost(normalize('angular.json')).toPromise();
-    if (!context.target) {
-      throw new Error('Cannot deploy the application without a target');
+    async (_: any, context: BuilderContext): Promise<BuilderOutput> => {
+        // TODO: handle failure
+        // The project root is added to a BuilderContext.
+        const root = normalize(context.workspaceRoot);
+        const workspace = new experimental.workspace.Workspace(root, new NodeJsSyncHost());
+        await workspace.loadWorkspaceFromHost(normalize('angular.json')).toPromise();
+        if (!context.target) {
+            throw new Error('Cannot deploy the application without a target');
+        }
+        const project = workspace.getProject(context.target.project);
+
+        const firebaseProject = getFirebaseProjectName(workspace.root, context.target.project);
+
+        return deploy(require('firebase-tools'), context, join(workspace.root, project.root), firebaseProject).then(() => ({success: true}));
     }
-    const project = workspace.getProject(context.target.project)
-    return deploy(require('firebase-tools'), context, join(workspace.root, project.root)).then(() => ({ success: true }));
-  }
 );
